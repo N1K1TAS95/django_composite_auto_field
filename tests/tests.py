665ed6cf0c7e9918cddb0106d3f1c6946b8e9ed2
Nikita Sinko <n.sinko95@gmail.com>
2023-04-11 10:28:25 +0200

    Fixed tests.py

--- tests/tests.py
+++ tests/tests.py
@@ -1,17 +1,9 @@
 from unittest.mock import patch
 
 from django.test import TestCase
-from django.utils import timezone
 from django.utils.timezone import now
 
 from tests.models import TestModelA, TestModelB
-
-
-def mock_now(year):
-    def inner_mock_now():
-        return timezone.datetime(year, 1, 1, tzinfo=timezone.utc)
-
-    return inner_mock_now
 
 
 class CompositeAutoFieldTestCase(TestCase):
@@ -39,21 +31,20 @@ class CompositeAutoFieldTestCase(TestCase):
         # Avanza l'anno corrente di un anno
         next_year = now().year + 1
 
-        # Utilizza un decoratore per sovrascrivere il metodo now() di timezone
-        with patch('django.utils.timezone.now', new=mock_now(next_year)):
+        with patch('django_composite_auto_field.fields.now') as mock_now:
+            mock_now.return_value = now().replace(year=next_year)
             # Crea un modello nell'anno successivo
             test_model_next_year = TestModelA()
             test_model_next_year.save()
 
             # Verifica che l'anno nel campo custom_code sia stato aggiornato
-            current_year = str(test_model_current_year.custom_code[2:4])
             next_year_str = str(test_model_next_year.custom_code[2:4])
             self.assertEqual(next_year_str, str(next_year)[2:])
 
             # Verifica che il contatore sia stato resettato
             counter_current_year = int(test_model_current_year.custom_code[4:])
             counter_next_year = int(test_model_next_year.custom_code[4:])
-            self.assertEqual(counter_current_year + 1, counter_next_year)
+            self.assertEqual(counter_current_year, counter_next_year)
 
     def test_zeros(self):
         # Crea e salva un'istanza del modello di test
@@ -88,3 +79,27 @@ class CompositeAutoFieldTestCase(TestCase):
         custom_code_value = 'AA230001'
         test_model_a = TestModelA.objects.create(custom_code=custom_code_value)
         self.assertEqual(test_model_a.custom_code, custom_code_value)
+
+    def test_code_sequence(self):
+        # Crea tre modelli
+        test_model_1 = TestModelA()
+        test_model_1.save()
+        test_model_2 = TestModelA()
+        test_model_2.save()
+        test_model_3 = TestModelA()
+        test_model_3.save()
+
+        # Estrai l'anno corrente e le parti numeriche dei codici generati
+        current_year = str(now().year)[2:]
+        num_1 = int(test_model_1.custom_code[4:])
+        num_2 = int(test_model_2.custom_code[4:])
+        num_3 = int(test_model_3.custom_code[4:])
+
+        # Verifica che i prefissi e gli anni siano corretti
+        self.assertEqual(test_model_1.custom_code[:4], f'AA{current_year}')
+        self.assertEqual(test_model_2.custom_code[:4], f'AA{current_year}')
+        self.assertEqual(test_model_3.custom_code[:4], f'AA{current_year}')
+
+        # Verifica che i numeri siano in sequenza
+        self.assertEqual(num_2, num_1 + 1)
+        self.assertEqual(num_3, num_2 + 1)
